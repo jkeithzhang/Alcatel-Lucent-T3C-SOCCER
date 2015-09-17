ee67626781fafedde1bbfef4ba3e1ff8f46e97ca
zhangkecyrus <kzhan013@uottawa.ca>
2015-09-17 12:23:15 -0400

    status bar and jpg logo added under root directory

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,9 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        console.log(StatusBar);
+        StatusBar.overlaysWebView(false);
+        StatusBar.styleDefault();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
