669ac16e6f8826924032a3705fd7a8d9101b10b4
zhangkecyrus <kzhan013@uottawa.ca>
2015-09-19 12:04:37 -0400

    panel become global

--- www/js/first-test.js
+++ www/js/first-test.js
@@ -7,3 +7,11 @@ var MyObj = function() {
 function myFunction(p1, p2) {
     return p1 * p2;              // The function returns the product of p1 and p2
 };
+
+var panel ='<div data-role="panel" data-theme="g" id="panel"><ul data-role="listview" data-theme="g" data-divider-theme="g"><li data-role="list-divider">Menu</li><li><a class="test" href="#">Next Game</a></li><li><a class="test" href="#">Availability</a></li><li data-role="list-divider">More Info</li><li><a class="test" href="#">Calendar</a></li><li><a class="test" href="#">Notification</a></li></ul></div>';
+
+$(document).on('pagebeforecreate', function () {
+    // if ($(this).find('[data-role=panel]').length === 0) {
+    $('[data-role=header]').before(panel);
+    // }
+});
