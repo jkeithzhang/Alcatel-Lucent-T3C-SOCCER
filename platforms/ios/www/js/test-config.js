3e78c095e5018155a403b6cd4ddb3697fe10678f
zhangkecyrus <kzhan013@uottawa.ca>
2015-09-29 00:06:54 -0400

    update

--- platforms/ios/www/js/test-config.js
+++ platforms/ios/www/js/test-config.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
             $.each(response, function (i, item) {
                 trHTML += '<tr><td style="text-align:center;vertical-align: middle">' + 
                 item.date + '</td><td style="text-align:center;vertical-align: middle">' + 
-                item.weekday + '</td><td><div data-role="fieldcontain"><fieldset data-role="controlgroup"><label><input type="checkbox" name="checkbox-0"/>In</label></fieldset></div></td></tr>';
+                item.weekday + '</td><td><div data-role="fieldcontain" style="margin-left: 30px"><fieldset data-role="controlgroup"><label><input type="checkbox" name="checkbox-0"/>In</label></fieldset></div></td></tr>';
             });
             console.log(trHTML);
             $(trHTML).appendTo('#records_table').trigger('create');
