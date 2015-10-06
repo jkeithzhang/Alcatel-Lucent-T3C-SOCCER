87ce7b4a456056d56f85e58e32d7cdd8ac6396cd
zhangkecyrus <kzhan013@uottawa.ca>
2015-10-06 17:22:42 -0400

    df

--- www/js/config.js
+++ www/js/config.js
@@ -2,7 +2,7 @@
 // Right now env is only for localhost (DEV only)
 // 
 var server = {
-	host_port: 'http://localhost:2000'
+	host_port: 'http://192.168.1.124:2000'
 	// user: 'root',
 	// password: 'tigris',
 	// database: 'tshelp',
