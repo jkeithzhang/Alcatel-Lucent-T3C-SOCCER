b3fd380ac4d2f5136afacb285509447c56a58edd
zhangkecyrus <kzhan013@uottawa.ca>
2015-10-07 18:56:35 -0400

    vclick

--- server/app.js
+++ server/app.js
@@ -24,10 +24,10 @@ io.on('connection', function(socket){
   // socket.on('disconnect', function(){
   //   console.log('user disconnected');
   // });
-
   socket.on('chat message', function(msg){
     // io.emit('chat message', msg);
-    socket.broadcast.emit('chat message', msg);
+    console.log('recieved');
+    io.emit('chat message', msg);
   });
 
   //broadcasting..
--- www/js/test-config.js
+++ www/js/test-config.js
@@ -11,7 +11,7 @@ $(function() {
             var trHTML = '';
             $.each(response, function (i, item) {
                 trHTML += '<tr><td style="text-align:center;vertical-align: middle">' + 
-                item.date + '</td><td><div data-role="fieldcontain" style="margin-left: 30px"><fieldset data-role="controlgroup"><label class="join"><input type="checkbox" class="custom"/>Join</label></fieldset></div></td></tr>';
+                item.date + '</td><td style="text-align:center;vertical-align: middle"><a class="in-button">I\'m in</a></td></tr>';
             });
             // console.log(trHTML);
             $(trHTML).appendTo('#records_table').trigger('create');
@@ -27,6 +27,7 @@ $(function() {
         }        
     });
 
+    // socket.emit('chat message', 'James joining');
     socket.on('chat message', function(msg){
         var insertHTML = '';
         insertHTML += '<div id="white-team" class="ui-bar ui-bar-a" style="text-align:center;">James</div>';
@@ -35,6 +36,14 @@ $(function() {
 
 });
 
+// $(document).on( "pageinit", "#page1", function( event ) {
+//     alert( "This page was just enhanced by jQuery Mobile!" );
+// });
+
+$(document).on( "vclick", ".in-button", function() {
+  $(this).html("joined");
+  socket.emit('chat message', 'James joining');
+});
 
 function onLoad() {
     document.addEventListener("deviceready", onDeviceReady, false);
