f7aa163b1339f035c55b7081b41a7138b60147e3
zhangkecyrus <kzhan013@uottawa.ca>
2015-11-01 12:29:51 -0500

    test on android

--- platforms/ios/www/js/config.js
+++ platforms/ios/www/js/config.js
@@ -2,7 +2,8 @@
 // Right now env is only for localhost (DEV only)
 // 
 var server = {
-	host_port: 'http://10.0.1.2:2000'
+	host_only: '192.168.1.102',
+	host_port: 'http://192.168.1.102:2000'
 	// user: 'root',
 	// password: 'tigris',
 	// database: 'tshelp',
